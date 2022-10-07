fa4b82626551acd896d0d0a0a74b410bce42f85f
MuhammadSabri1306 <muhammadsabri1306@gmail.com>
2022-10-07 09:14:10 +0800

    update home ui

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -26,7 +26,7 @@ onMounted(() => {
 		<div class="absolute-layer flex justify-center items-center z-[1]">
 			<div class="flip-wrapper">
 				<a href="/agenda" class="flip mx-auto">
-					<div class="flip-inner w-[20rem] md:w-[24rem] h-[22rem]">
+					<div class="flip-inner w-[20rem] md:w-[24rem] h-[24rem]">
 						<div class="flip-front">
 							<div class="rounded-3xl bg-white/80 shadow w-full h-full flex flex-col justify-center items-center">
 								<h2 class="text-3xl text-black text-center font-semibold font-body">Selamat Datang di</h2>
@@ -34,7 +34,8 @@ onMounted(() => {
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24"><path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" /></svg>
 								</div>
 								<h6 class="text-lg text-black text-center font-body mb-2">SISTEM INFORMASI</h6>
-								<h1 class="text-5xl text-red-500 font-black text-shadow-white">E-AGENDA</h1>
+								<h1 class="text-5xl text-red-500 font-black text-shadow-white mb-4">E-AGENDA</h1>
+								<p class="text-sm text-white font-bold bg-indigo-600 px-4 py-2 rounded-[50rem]">MULAI</p>
 							</div>
 						</div>
 						<div class="flip-back">
